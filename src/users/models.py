3180851a6c4100fbe9067706a4d6dde22de6b102
smilecool2012 <smilecool2012@gmail.com>
2022-06-11 15:57:36 +0300

    Change css

--- src/users/models.py
+++ src/users/models.py
@@ -3,9 +3,3 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-class AssistantUser(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-
-    class Meta:
-        verbose_name = 'User'
-        verbose_name_plural = 'Users'
